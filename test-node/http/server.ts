d36bb0d50f41b6f232ae230ae030e0a40c0f8f4d
Seratooo <pauloserato2016@gmail.com>
2021-05-11 21:08:37 +0100

    fs.Read

--- test-node/http/server.ts
+++ test-node/http/server.ts
@@ -1,6 +1,25 @@
 import http from 'http'
+import fs from 'fs'
+import path from 'path'
 
-http.createServer((req,res)=>{
-  res.write('Hello Word')
-  res.end()
-}).listen(5000)
+
+http.createServer((req, res) => {
+
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public', file)
+    const extname = path.extname(filePath)
+
+    const allowedFileTypes = ['.html', '.css', '.js']
+    const allowed = allowedFileTypes.find(item => item == extname)
+
+    if(!allowed) return
+
+    fs.readFile(
+        filePath,
+        (err, content) => {
+            if(err) throw err
+
+            res.end(content)
+        }
+    )
+}).listen(5000, () => console.log('Server is running')) 
